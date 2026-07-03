96d2626261a337352617226cefd90625048eb3e7
Rahul Mohandas <rahul0705@gmail.com>
2026-07-03 15:35:55 -0400

    feat(lint) added eslint

---
+++ eslint.config.ts
@@ -0,0 +1,9 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    ...eslintPluginAstro.configs.recommended,
+    {
+        rules: {},
+    },
+]);
