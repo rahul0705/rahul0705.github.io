f36f46c5e64036ff294d9596c73f3833f142d30b
Rahul Mohandas <rahul0705@gmail.com>
2026-07-04 16:41:40 -0400

    fix(spell) added dictionary

---
+++ cspell.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'cspell';
+
+export default defineConfig({
+  ignorePaths: ['node_modules'],
+  words: [
+    'Ansible',
+    'Arroz',
+    'Clearcase',
+    'cooldown',
+    'Decompiling',
+    'EVMS',
+    'Himawari',
+    'keepalive',
+    'Killian',
+    'linkedin',
+    'pudevelopers',
+    'Rahul',
+    'raspberrypi',
+    'RFIMS',
+    'vexrobotics',
+    'zizmor',
+  ],
+});
