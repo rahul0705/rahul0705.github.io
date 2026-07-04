bff764df5f52794ecbf184bcf225c4ceb77f3d49
Rahul Mohandas <rahul0705@gmail.com>
2026-07-04 08:22:53 -0400

    feat(ci) added new workflow for lint and format

--- eslint.config.ts
+++
@@ -1,9 +0,0 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-import { defineConfig } from 'eslint/config';
-
-export default defineConfig([
-  ...eslintPluginAstro.configs.recommended,
-  {
-    rules: {},
-  },
-]);
