b16e168ba1abb5fc4a5c6d8adf5578c07a3b8d26
Rahul Mohandas <rahul0705@gmail.com>
2026-08-05 19:42:51 -0400

    chore(sort) sort items

--- src/data/resume/skills.ts
+++ src/data/resume/skills.ts
@@ -6,28 +6,28 @@ export interface SkillCatalogEntry {
 }
 
 export const skillCatalog = {
-  java: {
-    name: 'Java',
+  agileScrum: {
+    name: 'Agile Scrum',
+    description: 'Software Development Methodology',
+    href: 'https://en.wikipedia.org/wiki/Scrum_(software_development)',
+  },
+  ansible: {
+    name: 'Ansible',
+    description: 'Automation Tool',
+    href: 'https://www.ansible.com/',
+    trackExperienceCoverage: true,
+  },
+  attentive: { name: 'Attentive', description: 'Careful attention to customer and operational needs.' },
+  aws: {
+    name: 'AWS',
+    description: 'Cloud Service Provider',
+    href: 'https://aws.amazon.com/',
+    trackExperienceCoverage: true,
+  },
+  c: {
+    name: 'C',
     description: 'Programming Language',
-    href: 'https://www.java.com/en/',
-    trackExperienceCoverage: true,
-  },
-  typescript: {
-    name: 'TypeScript',
-    description: 'Programming Language',
-    href: 'https://www.typescriptlang.org/',
-    trackExperienceCoverage: true,
-  },
-  react: {
-    name: 'React',
-    description: 'JavaScript Library',
-    href: 'https://react.dev/',
-    trackExperienceCoverage: true,
-  },
-  cypress: {
-    name: 'Cypress',
-    description: 'JavaScript End-to-End Testing Library',
-    href: 'https://www.cypress.io/',
+    href: 'https://www.open-std.org/jtc1/sc22/wg14/',
     trackExperienceCoverage: true,
   },
   cdk: {
@@ -36,44 +36,43 @@ export const skillCatalog = {
     href: 'https://aws.amazon.com/cdk/',
     trackExperienceCoverage: true,
   },
-  aws: {
-    name: 'AWS',
-    description: 'Cloud Service Provider',
-    href: 'https://aws.amazon.com/',
+  clearCase: {
+    name: 'ClearCase',
+    description: 'Software Configuration Management',
+    href: 'https://www.ibm.com/products/rational-clearcase',
     trackExperienceCoverage: true,
   },
-  agileScrum: {
-    name: 'Agile Scrum',
-    description: 'Software Development Methodology',
-    href: 'https://en.wikipedia.org/wiki/Scrum_(software_development)',
+  codeReviews: { name: 'Code Reviews', description: 'Collaborative source-code quality review.' },
+  computerScience: {
+    name: 'Computer Science',
+    description: 'Study of computation, algorithms, and software systems.',
+    href: 'https://www.cs.purdue.edu/',
+  },
+  costSavingsAnalysis: {
+    name: 'Cost Savings Analysis',
+    description: 'Evaluation of cost and efficiency improvements.',
+  },
+  customerRelations: { name: 'Customer Relations', description: 'Supporting customers and understanding their needs.' },
+  cypress: {
+    name: 'Cypress',
+    description: 'JavaScript End-to-End Testing Library',
+    href: 'https://www.cypress.io/',
+    trackExperienceCoverage: true,
+  },
+  designDocumentation: { name: 'Design Documentation', description: 'Technical design communication.' },
+  distributedSystems: {
+    name: 'Distributed Systems',
+    description: 'Systems that coordinate work across multiple machines.',
   },
   evms: {
     name: 'EVMS',
     description: 'Project Management Technique',
     href: 'https://en.wikipedia.org/wiki/Earned_value_management',
   },
-  ansible: {
-    name: 'Ansible',
-    description: 'Automation Tool',
-    href: 'https://www.ansible.com/',
-    trackExperienceCoverage: true,
-  },
-  python: {
-    name: 'Python',
-    description: 'Programming Language',
-    href: 'https://www.python.org/',
-    trackExperienceCoverage: true,
-  },
-  javascript: {
-    name: 'JavaScript',
-    description: 'Programming Language',
-    href: 'https://www.javascript.com/',
-    trackExperienceCoverage: true,
-  },
-  nodejs: {
-    name: 'Node.js',
-    description: 'JavaScript Runtime',
-    href: 'https://nodejs.org/',
+  flask: {
+    name: 'Flask',
+    description: 'Python Framework',
+    href: 'https://flask.palletsprojects.com/',
     trackExperienceCoverage: true,
   },
   git: {
@@ -88,22 +87,23 @@ export const skillCatalog = {
     href: 'https://groovy-lang.org/',
     trackExperienceCoverage: true,
   },
+  java: {
+    name: 'Java',
+    description: 'Programming Language',
+    href: 'https://www.java.com/en/',
+    trackExperienceCoverage: true,
+  },
+  javascript: {
+    name: 'JavaScript',
+    description: 'Programming Language',
+    href: 'https://www.javascript.com/',
+    trackExperienceCoverage: true,
+  },
+  leadership: { name: 'Leadership', description: 'Guiding teams and coordinating work.' },
   magicDraw: {
     name: 'MagicDraw',
     description: 'Software Modeling Tool',
     href: 'https://www.3ds.com/products-services/catia/products/no-magic/magicdraw/',
-    trackExperienceCoverage: true,
-  },
-  c: {
-    name: 'C',
-    description: 'Programming Language',
-    href: 'https://www.open-std.org/jtc1/sc22/wg14/',
-    trackExperienceCoverage: true,
-  },
-  rabbitmq: {
-    name: 'RabbitMQ',
-    description: 'Messaging Queue',
-    href: 'https://www.rabbitmq.com/',
     trackExperienceCoverage: true,
   },
   netcdf: {
@@ -112,16 +112,15 @@ export const skillCatalog = {
     href: 'https://www.unidata.ucar.edu/software/netcdf/',
     trackExperienceCoverage: true,
   },
-  flask: {
-    name: 'Flask',
-    description: 'Python Framework',
-    href: 'https://flask.palletsprojects.com/',
-    trackExperienceCoverage: true,
+  networkAdministration: {
+    name: 'Network Administration',
+    description: 'Administration of network systems and services.',
   },
-  clearCase: {
-    name: 'ClearCase',
-    description: 'Software Configuration Management',
-    href: 'https://www.ibm.com/products/rational-clearcase',
+  networkTesting: { name: 'Network Testing', description: 'Validation of network behavior and reliability.' },
+  nodejs: {
+    name: 'Node.js',
+    description: 'JavaScript Runtime',
+    href: 'https://nodejs.org/',
     trackExperienceCoverage: true,
   },
   oracleCoherence: {
@@ -130,53 +129,54 @@ export const skillCatalog = {
     href: 'https://www.oracle.com/middleware/technologies/coherence.html',
     trackExperienceCoverage: true,
   },
-  tcpIp: { name: 'TCP/IP', description: 'Internet protocol suite.' },
-  distributedSystems: {
-    name: 'Distributed Systems',
-    description: 'Systems that coordinate work across multiple machines.',
-  },
+  organization: { name: 'Organization', description: 'Planning and coordinating work.' },
   osProgramming: { name: 'OS Programming', description: 'Operating system-level software development.' },
-  networkTesting: { name: 'Network Testing', description: 'Validation of network behavior and reliability.' },
-  softwareTesting: { name: 'Software Testing', description: 'Validation of software quality and behavior.' },
-  workInstructions: { name: 'Work Instructions', description: 'Operational and implementation documentation.' },
-  designDocumentation: { name: 'Design Documentation', description: 'Technical design communication.' },
-  codeReviews: { name: 'Code Reviews', description: 'Collaborative source-code quality review.' },
-  presentationProficiency: {
-    name: 'Presentation Proficiency',
-    description: 'Clear communication of technical work to stakeholders.',
-  },
-  costSavingsAnalysis: {
-    name: 'Cost Savings Analysis',
-    description: 'Evaluation of cost and efficiency improvements.',
-  },
   perl: {
     name: 'Perl',
     description: 'Programming Language',
     href: 'https://www.perl.org/',
     trackExperienceCoverage: true,
   },
-  leadership: { name: 'Leadership', description: 'Guiding teams and coordinating work.' },
-  organization: { name: 'Organization', description: 'Planning and coordinating work.' },
+  presentationProficiency: {
+    name: 'Presentation Proficiency',
+    description: 'Clear communication of technical work to stakeholders.',
+  },
+  python: {
+    name: 'Python',
+    description: 'Programming Language',
+    href: 'https://www.python.org/',
+    trackExperienceCoverage: true,
+  },
+  rabbitmq: {
+    name: 'RabbitMQ',
+    description: 'Messaging Queue',
+    href: 'https://www.rabbitmq.com/',
+    trackExperienceCoverage: true,
+  },
+  react: {
+    name: 'React',
+    description: 'JavaScript Library',
+    href: 'https://react.dev/',
+    trackExperienceCoverage: true,
+  },
   recruiting: { name: 'Recruiting', description: 'Hiring and candidate evaluation.' },
-  training: { name: 'Training', description: 'Onboarding and skill development.' },
-  attentive: { name: 'Attentive', description: 'Careful attention to customer and operational needs.' },
+  softwareMaintenance: { name: 'Software Maintenance', description: 'Ongoing support and upkeep of software systems.' },
+  softwareTesting: { name: 'Software Testing', description: 'Validation of software quality and behavior.' },
+  tcpIp: { name: 'TCP/IP', description: 'Internet protocol suite.' },
+  teamCoordination: { name: 'Team Coordination', description: 'Coordinating work across people and teams.' },
   ticketSystems: { name: 'Ticket Systems', description: 'Tracking and resolving support requests.' },
+  training: { name: 'Training', description: 'Onboarding and skill development.' },
+  typescript: {
+    name: 'TypeScript',
+    description: 'Programming Language',
+    href: 'https://www.typescriptlang.org/',
+    trackExperienceCoverage: true,
+  },
   windowsAdministration: {
     name: 'Windows Administration',
     description: 'Administration of Windows systems and domains.',
   },
-  networkAdministration: {
-    name: 'Network Administration',
-    description: 'Administration of network systems and services.',
-  },
-  softwareMaintenance: { name: 'Software Maintenance', description: 'Ongoing support and upkeep of software systems.' },
-  teamCoordination: { name: 'Team Coordination', description: 'Coordinating work across people and teams.' },
-  customerRelations: { name: 'Customer Relations', description: 'Supporting customers and understanding their needs.' },
-  computerScience: {
-    name: 'Computer Science',
-    description: 'Study of computation, algorithms, and software systems.',
-    href: 'https://www.cs.purdue.edu/',
-  },
+  workInstructions: { name: 'Work Instructions', description: 'Operational and implementation documentation.' },
 } as const satisfies Record<string, SkillCatalogEntry>;
 
 export type SkillId = keyof typeof skillCatalog;
