28eb7cd4cc8b43aba553851422d1af6ba734161a
Rahul Mohandas <rahul0705@gmail.com>
2026-08-13 13:55:09 -0400

    Fix Astro adapter formatting

--- src/integrations/astro/adapter.ts
+++ src/integrations/astro/adapter.ts
@@ -44,9 +44,9 @@ type AstroFieldOutputByKind<Field extends ContentField> = {
 type AstroFieldOutput<Field extends ContentField> = AstroFieldOutputByKind<Field>[Field['kind']];
 
 type AstroSchemaShape<Model extends ContentCollectionModel> = {
-  [Key in keyof Model['fields'] as Model['fields'][Key]['kind'] extends 'body'
-    ? never
-    : Model['fields'][Key]['name']]: z.ZodType<AstroFieldOutput<Model['fields'][Key]>>;
+  [
+    Key in keyof Model['fields'] as Model['fields'][Key]['kind'] extends 'body' ? never : Model['fields'][Key]['name']
+  ]: z.ZodType<AstroFieldOutput<Model['fields'][Key]>>;
 };
 
 const createAstroField = (field: ContentField, image: SchemaContext['image']): z.ZodType<unknown> => {
